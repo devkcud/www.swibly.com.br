c31c39e4df59b7320d5b5932350352fe70f2ba35
André devkcud Albanese Junior <patommmmm@proton.me>
2024-10-10 17:36:30 -0300

    Add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import icon from 'astro-icon';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: vercel()
 });
